f3fcf64c835d812a32a6a9e0cf250ab77bfbb8fa
Fredrik Nyberg <fredriknyberg1@gmail.com>
2020-10-20 15:50:19 +0200

    Look for time (part)

--- test.py
+++ test.py
@@ -1,16 +1,33 @@
 import csv
 import pandas as pd
-import time
+from datetime import datetime, time, date
+
+tid = datetime.now().time()
+datum = datetime.now().date()
+dag = datum.strftime("%A")
+
+print(tid)
 
 df = pd.read_csv('taggar.csv')
+dl = pd.read_csv('schema.csv')
 
-tag = int(input("Input tag: "))
+# tag = int(input("Input tag: "))
 
 i = 0
 
 klasser = df.columns
+tider = dl.columns
 
 tags = []
+
+klass_Ek20 = df[klasser[0]].to_list()
+klass_Na20 = df[klasser[1]].to_list()
+
+print(dl[tider[0]].to_list())
+
+
+
+klass_lunch = []
 
 
 for klass in klasser:
@@ -21,10 +38,18 @@ for klass in klasser:
     # print(tags)
     i += 1
 
-if (tag in tags):
-    print("POG")
-    if (tag in klass_lunch):
-        print("Du får äta")
-    else: 
-        print("X")
 
+
+if (tid >= dl[tider[0]].to_list()):
+    print("Burh")
+    
+#     if (tag in klass_Na20):
+#         print("Du får äta nu")
+#     else:
+#         print("Du får inte äta nu.mp3")
+
+# if (tag in klass_Na20):
+#     if (tid > tid.replace(hour=11, minute=0, second=0, microsecond=0)):
+#         print("Du får äta nu")
+#     else:
+#         print("Du får inte äta nu.mp3")
