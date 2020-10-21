fc64fdde47608ad212c45ff3d2874fc813bd4732
Klado555 <klaudiusz.pawezka@elev.ntig.se>
2020-10-21 15:51:21 +0200

    removed not needed files

--- lunchtag_test.py
+++
@@ -1,8 +0,0 @@
-import csv
-import pandas
-
-with open('tags_20201012.csv') as file:
-    spamreader = csv.reader(file, delimiter=' ', quotechar='|')
-    reader = csv.reader(file)
-    for row in reader:
-        pandas
