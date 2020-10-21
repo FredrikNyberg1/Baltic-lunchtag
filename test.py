d522be951572a179d7ea8c712727cae9b4848f19
Fredrik Nyberg <fredriknyberg1@gmail.com>
2020-10-21 15:47:48 +0200

    Can look for tags in system

--- test.py
+++ test.py
@@ -1,56 +1,24 @@
 import csv
 import pandas as pd
-from datetime import datetime, time, date
-
-tid = datetime.now().time()
-datum = datetime.now().date()
-dag = datetime.now().weekday()
-
-print(tid)
-print(dag)
 
 taggar_csv = pd.read_csv('taggar.csv')
-schema_csv = pd.read_csv('schema.csv')
 
 i = 0
 
 klasser = taggar_csv.columns
-tider = schema_csv.columns
-# dagar = schema_csv.iloc[]
-
-
-# print(dagar)
 
 tags = []
 
 klass_lunch = []
 
-# for klass in klasser:
-#     # print(klasser[i])
-#     klass_tags = taggar_csv[klasser[i]].to_list()
-#     # print(klass_tags)
-#     tags.extend(klass_tags)
-#     # print(tags)
-#     i += 1
+for klass in klasser:
+    klass_tags = taggar_csv[klasser[i]].to_list()
+    tags.extend(klass_tags)
+    i += 1
 
-i = 0
-
-
-
-
-lunchdag = schema_csv.iloc[dag, 0]
-print(lunchdag)
-tag = int(input("Input tag: "))
-
-# for tag in tags:
-
-
-#         print("Du får äta nu!")
-#     else:
-#         print("Du får inte äta nu")
-                
-# if (tag in klass_Na20):
-#     if (tid > tid.replace(hour=11, minute=0, second=0, microsecond=0)):
-#         print("Du får äta nu")
-#     else:
-#         print("Du får inte äta nu.mp3")
+for tag in tags:
+    tag = int(input("Input tag: "))
+    if tag in tags:
+        print("Du får äta här!")
+    else:
+        print("Du får inte äta här...")
